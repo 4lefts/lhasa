8bad79501a4678b6b2016f2fbc3888f5619c33ea
Stephen Ball <5t3ph3n.ball@gmail.com>
2017-05-31 08:30:03 +0100

    refactored functions

--- src/app.js
+++ src/app.js
@@ -1,34 +1,40 @@
 const p5 = require('p5')
-const tone = require('tone')
+const Tone = require('tone')
 
-const lhasa = new p5((l) => {
+const lhasa = new p5(function(l){
 
-	let sz = window.innerHeight < window.innerWidth ? window.innerHeight : window.innerWidth
+	const padding = 20
+	let sz = window.innerHeight < window.innerWidth ? window.innerHeight - padding : window.innerWidth - padding
 
-	l.setup = () => {
+	l.setup = function(){
 		l.createCanvas(window.innerWidth, window.innerHeight)
 	}
 
-	l.draw = () => {
+	l.draw = function(){
 		l.background(0)
 		l.noStroke()
 		l.fill(200)
 		l.ellipse(l.width/2, l.height/2, sz -10, sz -10)
 	}
 
-	l.windowResized = () => {
-		sz = window.innerHeight < window.innerWidth ? window.innerHeight : window.innerWidth
+	l.windowResized = function(){
+		sz = window.innerHeight < window.innerWidth ? window.innerHeight - padding : window.innerWidth - padding
 		l.resizeCanvas(window.innerWidth, window.innerHeight)
 	}
 
-	l.mousePressed = () => {
+	l.mousePressed = function(){
 		if(l.dist(l.mouseX, l.mouseY, l.width/2, l.height/2) < sz - 10){
 			triggerSynth()
 		} 
 	}	
-
-	const triggerSynth = () => {
-		const synth = new tone.Synth().toMaster()
+/*
+	drawCircles = function(){
+		const r = sz / 4
+		
+	}
+*/
+	function triggerSynth(){
+		const synth = new Tone.Synth().toMaster()
 		synth.triggerAttackRelease('C4', '8n')
 	}
 

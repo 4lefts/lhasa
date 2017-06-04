2de486c0ea5684241ec15c07a76b1ec627f7454d
Stephen Ball <5t3ph3n.ball@gmail.com>
2017-06-04 20:25:23 +0100

    added loop select functionality

--- src/app.js
+++ src/app.js
@@ -4,10 +4,17 @@ const Tone = require('tone')
 const lhasa = new p5(function(l){
 
 	const padding = 10
-		let lightGrey, midGrey, darkGray, blue, blueAlpha
+	let lightGrey, midGrey, midGreyAlpha, darkGray, blue, blueAlpha
 
+	//variables for drawing waveform
 	let waveformArray
 	let loaded = false
+	//and controlling mouse behaviour for editing loop points
+	let initialPressPoint = 0
+	let tmpLoopEnd = 0
+	let finalPressPoint = 0
+	let pMouseIsPressed = false
+	let editable = true
 
 	const	soundPlayer = new Tone.Player('./gtr.mp3', function(){
 			soundPlayer.loop = true
@@ -17,10 +24,6 @@ const lhasa = new p5(function(l){
 			waveformArray = computeWaveform(soundPlayer.buffer.getChannelData(0), window.innerWidth, window.innerHeight)
 			loaded = true
 			console.log(`sample length (seconds): ${soundPlayer.buffer.duration}`)
-			console.log(`sample length (samples): ${soundPlayer.buffer.length}`)
-			console.log(`waveformArray length: ${waveformArray.length}`)
-			console.log(`window width: ${window.innerWidth}`)
-			console.log(`waveformArray data: ${waveformArray}`)
 		}).toMaster()
 
 
@@ -29,9 +32,13 @@ const lhasa = new p5(function(l){
 		cnv.mousePressed(function(){
 			if(loaded) checkPress(l.mouseX, l.mouseY)
 		})
+		cnv.mouseReleased(function(){
+			editable = true
+		})
 		lightGrey = l.color(217, 225, 232)
 		midGrey = l.color(155, 174, 200)
-		darkGray = l.color(40,  44,  55)
+		midGreyAlpha = l.color(155, 174, 200, 100)
+		darkGray = l.color(40, 44, 55)
 		blue = l.color(43, 144, 217)
 		blueAlpha = l.color(43, 144, 217, 100)
 	}
@@ -39,9 +46,9 @@ const lhasa = new p5(function(l){
 	l.draw = function(){
 		l.background(darkGray)
 		if(loaded) {
+			if(editable) checkLoopPointEdit()
 			drawWaveform(waveformArray, l.height)
 			drawLoopPoints(soundPlayer.loopStart, soundPlayer.loopEnd, soundPlayer.buffer.duration)
-			console.log(soundPlayer.loopStart)
 			drawPlayButton(soundPlayer.state)
 		}	else {
 			drawLoading()
@@ -55,8 +62,23 @@ const lhasa = new p5(function(l){
 
 	function checkPress(x, y){
 		if(x > 10 && x < 60 && y > 10 && y < 60){
+			editable = false //stop button updating loop points
 			startStop(soundPlayer.state)
 		}
+	}
+
+	function checkLoopPointEdit(){
+		if(l.mouseIsPressed && !pMouseIsPressed){
+			initialPressPoint = l.mouseX
+		}
+		if(l.mouseIsPressed && pMouseIsPressed){
+			tmpLoopEnd = l.mouseX
+			drawEditPoints(initialPressPoint, tmpLoopEnd)
+		}
+		if(!l.mouseIsPressed && pMouseIsPressed){
+			updateLoopPoints(initialPressPoint, tmpLoopEnd)
+		}
+		pMouseIsPressed = l.mouseIsPressed
 	}
 
 	function startStop(state){
@@ -75,6 +97,7 @@ const lhasa = new p5(function(l){
 		return ret
 	}
 
+	//gui draw functions
 	function drawWaveform(arr, h){
 		l.push()
 		l.translate(0, h / 2)
@@ -97,7 +120,6 @@ const lhasa = new p5(function(l){
 		l.fill(lightGrey)
 		l.text('loading sound', l.width/2, l.height/2)
 		l.pop()
-		console.log('loading!')
 	}
 
 	function drawLoopPoints(s, e, tot){
@@ -109,6 +131,21 @@ const lhasa = new p5(function(l){
 		l.fill(blueAlpha)
 		l.rect(0, 0, x2, l.height)
 		l.stroke(blue)
+		l.strokeWeight(2)
+		l.line(0, 0, 0, l.height)
+		l.line(x2, 0, x2, l.height)
+		l.pop()
+	}
+
+	function drawEditPoints(p1, p2){
+		const x1 = p1 < p2 ? p1 : p2
+		const x2 = p2 > p1 ? p2 - x1 : p1 - x1
+		l.push()
+		l.translate(x1, 0)
+		l.noStroke()
+		l.fill(midGreyAlpha)
+		l.rect(0, 0, x2, l.height)
+		l.stroke(midGrey)
 		l.strokeWeight(2)
 		l.line(0, 0, 0, l.height)
 		l.line(x2, 0, x2, l.height)
@@ -133,9 +170,28 @@ const lhasa = new p5(function(l){
 		l.pop()
 	}
 
+	//synth functions
 	function triggerSynth(){
 		const synth = new Tone.Synth().toMaster()
 		synth.triggerAttackRelease('C4', '8n')
+	}
+
+	function updateLoopPoints(p1, p2){
+		console.log(p1, p2)
+		let t1, t2
+		if(p1 === p2) {
+			t1 = l.map(p1, 0, l.width, 0, soundPlayer.buffer.duration)
+			t2 = l.map(p2 + 1, 0, l.width, 0, soundPlayer.buffer.duration)
+		}	else if(p1 < p2){
+			t1 = l.map(p1, 0, l.width, 0, soundPlayer.buffer.duration)
+			t2 = l.map(p2, 0, l.width, 0, soundPlayer.buffer.duration)
+		} else {
+			t1 = l.map(p2, 0, l.width, 0, soundPlayer.buffer.duration)
+			t2 = l.map(p1, 0, l.width, 0, soundPlayer.buffer.duration)
+		}
+
+		soundPlayer.setLoopPoints(t1, t2)
+		soundPlayer.seek(t1)
 	}
 
 }, 'lhasa-container')
